216f7806c84d10f5a9c57789077f73f4ce05c9d4
NaomideJong <562748@student.inholland.nl>
2021-12-22 20:28:48 +0100

    partII

---
+++ assignment1/ChessGame.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment1
+{
+    class ChessGame
+    {
+        public void InitChessboard(ChessPiece[,] chessboard)
+        {
+            for (int row = 0; row < chessboard.GetLength(0); row++)
+            {
+                for (int col = 0; col < chessboard.GetLength(1); col++)
+                {
+                }
+            }
+        }
+
+        public void DisplayChessboard(ChessPiece[,] chessboard)
+        {
+            string square = " ";
+            int colCounter = chessboard.GetLength(1);
+
+            for (int row = 0; row < chessboard.GetLength(0); row++)
+            {
+                for (int col = 0; col < chessboard.GetLength(1) + 1; col++)
+                {
+                    int evenUneven = (row + col + 1) % 2;
+                    if (col == 0)
+                    {
+                        Console.Write($"{colCounter} ");
+                        colCounter--;
+                    }
+                    else if (evenUneven == 0)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Gray;
+                        Console.Write($"{square,3}");
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkYellow;
+                        Console.Write($"{square,3}");
+                    }
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("   a  b  c  d  e  f  g  h");
+        }
+
+        public void PutChessPieces(ChessPiece[,] chessboard)
+        {
+            ChessPieceType[] order =
+                {ChessPieceType.Rook, ChessPieceType.Knight, ChessPieceType.Bishop, ChessPieceType.Queen,
+                ChessPieceType.King, ChessPieceType.Bishop, ChessPieceType.Knight, ChessPieceType.Rook };
+
+            int whitePawns = 1;
+            int blackPawns = 6;
+            int firstRow = 0;
+            int lastRow = chessboard.GetLength(0);
+            int counter = 0;
+
+            for (int row = 0; row < chessboard.GetLength(0); row++)
+            {
+                for (int col = 1; col < chessboard.GetLength(1) + 1; col++)
+                {
+                    if (row == 0)
+                    {
+                        //chessboard.type[row, col] = order[counter];
+                        counter++;
+                    }
+                    else if (row == whitePawns)
+                    {
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ assignment1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace assignment1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Program myProgram = new Program();
+            myProgram.Start();
+        }
+        void Start()
+        {
+            ChessPiece[,] chessboard = new ChessPiece[8, 8];
+            ChessGame chessGame = new ChessGame();
+            //ChessGame.InitChessboard(chessboard);
+            //ChessGame.DisplayChessboard(chessboard);
+        }
+    }
+}
